e748252577c245eacba28854c1603fe53eea4b25
Aminul <mdaminulislamsahin14@gmail.com>
2022-10-22 13:11:03 +0600

    server done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 5000;
 
 app.use(cors());
 const hotels = require('./data/hotels.json')
+const locations = require('./data/locations.json')
 
 app.get("/", (req, res) => {
   res.send("Tour bd Server Running...");
@@ -12,6 +13,22 @@ app.get("/", (req, res) => {
 
 app.get('/hotels', (req, res) => {
     res.send(hotels)
+})
+
+app.get("/hotels/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedHotel = hotels.find(h => h.id === id)
+    res.send(selectedHotel)
+})
+
+app.get('/locations', (req, res) => {
+    res.send(locations)
+})
+
+app.get('/locations/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const selectedLocation = locations.find(l => l.id === id);
+    res.send(selectedLocation)
 })
 
 app.listen(port, () => {
